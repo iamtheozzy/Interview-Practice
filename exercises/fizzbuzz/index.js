5e833716c6e1f3d563a14eb4272dbe98b7617e48
Ozzy Cardenas <ocardenas789@me.com>
2018-07-18 22:39:53 -0500

    solved fizzbuzz problem

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -12,6 +12,22 @@
 //   4
 //   buzz
 
-function fizzBuzz(n) {}
+function fizzBuzz(n) {
+
+    for(let i = 1; i <=n; i++){
+        //Is the number a multiple of 3 and 5?
+        if(i % 3 === 0 && i % 5 === 0){
+            console.log('fizzbuzz');
+        } else if (i % 3 === 0){
+            console.log('fizz');
+        } else if (i % 5 === 0){
+            console.log('buzz');
+        } else {
+            console.log(i);
+        }
+    }
+
+
+}
 
 module.exports = fizzBuzz;
